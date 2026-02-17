79c4e3c899031632309f0d356e9e3c30e703ea02
just-jul <businessbyjul@gmail.com>
2026-02-17 12:21:04 +0100

    Initial commit

---
+++ src/main/java/com/just_jul/algorithms/AlgorithmVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.just_jul.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmVisualizerApplication.class, args);
+	}
+
+}
